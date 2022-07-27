f038d8e1ef46d55f6db3b108891598cc76386470
LaughingThroll <LaughingThroll@gmail.com>
2022-07-27 10:47:25 +0300

    add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
-export const App = () => {
-  return <>hello</>;
-};
+import React from "react";
+import "./app.css";
+
+export class App extends React.Component {
+  state = {
+    counter: 0,
+  };
+
+  increment = () => {
+    this.setState({ ...this.state, counter: this.state.counter + 1 });
+  };
+
+  // Неккорктно работает функция double поправь пожалуйста
+  double = () => {
+    this.increment();
+    this.increment();
+  };
+
+  render() {
+    return (
+      <div className="parent">
+        <div className="counter">{this.state.counter}</div>
+        <button className="button" onClick={this.double}>
+          Жмяк
+        </button>
+      </div>
+    );
+  }
+}
